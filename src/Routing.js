4c134dd916a9fbb5f71efe2b324d33e29348556e
shakilkhan12 <shakilkhan621@gmail.com>
2022-11-15 18:41:35 +0500

    show cart

--- src/Routing.js
+++ src/Routing.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import Home from "./pages/Home";
 import Nav from "./components/Nav";
+import toast, { Toaster } from "react-hot-toast";
 import Details from "./pages/Details";
 import Cart from "./pages/Cart";
 const Routing = () => {
@@ -10,11 +11,15 @@ const Routing = () => {
     const product = cart.find((pr) => pr.id === data.id);
     if (!product) {
       setCart([...cart, data]);
+      toast.success("You have added product in the cart");
+    } else {
+      toast.error("Product is already in the cart!");
     }
   };
   return (
     <BrowserRouter>
       <Nav cart={cart} />
+      <Toaster position="top-left" />
       <div className="max-w-screen-lg mx-auto px-6">
         <Routes>
           <Route path="/" element={<Home />} />
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,48 +1,29 @@
+import CartItems from "../components/CartItems";
 const Cart = ({ items }) => {
   return (
-    <div className="my-5">
+    <div className="my-5 overflow-x-auto">
       {items.length > 0 ? (
-        <table className="border w-full">
+        <table className="w-full">
           <thead>
             <tr>
-              <th className="border p-3 text-left uppercase text-sm font-medium">
+              <th className="bg-indigo-100 p-3 text-left uppercase text-sm font-medium">
                 image
               </th>
-              <th className="border p-3 text-left uppercase text-sm font-medium">
+              <th className="bg-indigo-100 p-3 text-left uppercase text-sm font-medium">
                 name
               </th>
-              <th className="border p-3 text-left uppercase text-sm font-medium">
+              <th className="bg-indigo-100 p-3 text-left uppercase text-sm font-medium">
                 price
               </th>
-              <th className="border p-3 text-left uppercase text-sm font-medium">
+              <th className="bg-indigo-100 p-3 text-left uppercase text-sm font-medium">
                 quantities
               </th>
-              <th className="border p-3 text-left uppercase text-sm font-medium">
+              <th className="bg-indigo-100 p-3 text-left uppercase text-sm font-medium">
                 delete
               </th>
             </tr>
           </thead>
-          <tbody>
-            {items.map((product) => (
-              <tr key={product.id} className="odd:bg-gray-50">
-                <td className="border p-3 text-left">
-                  <img
-                    src={product.image}
-                    alt={product.title}
-                    className="w-12 h-12 object-cover rounded"
-                  />
-                </td>
-                <td className="border p-3 text-left">{product.title}</td>
-                <td className="border p-3 text-left">{product.price}</td>
-                <td className="border p-3 text-left">{product.quantities}</td>
-                <td className="border p-3 text-left">
-                  <button className="bg-rose-600 px-5 py-2 text-white rounded cursor-pointer capitalize text-sm font-bold">
-                    delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
+          <CartItems items={items} />
         </table>
       ) : (
         <p className="bg-rose-50 text-rose-800 border-rose-100 rounded px-4 py-3 font-medium">
